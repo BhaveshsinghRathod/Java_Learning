2c658e56ebe103e1a488a8e524024348340d78d7
Bhaveshsingh Rathod <rathodbhaveshsingh519@gmail.com>
2022-05-21 16:01:15 +0530

    Basic intro to operators

---
+++ src/operatorss.java
@@ -0,0 +1,20 @@
+public class operatorss {
+    public static void main(String[] args){
+        /*
+        We will learn about the operators today
+        1) Comparing Operator :
+            --> Are used to compare two value.
+            --> A==B    A is equal to B
+            --> A!=B    A is not equal to B
+            --> A>B     A is greater than B
+            --> A<B     A is smaller than B
+            --> A>=B    A is greater or equal to B
+            --> A<=B    A is smaller or equal to B
+        2) Logical Operator
+            --> Is used to find the logical values between values
+            --> A==B && A<5 : Return true if the two conditions are met   [and]
+            --> A<B || A<C : Return true if one of the conditions in met   [or]
+            --> !(A<B) : Return false if the result is true and visa versa
+        */
+    }
+}
