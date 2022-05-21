e42a4d86119dedb368d16a882411b0a03a6d2d96
Bhaveshsingh Rathod <rathodbhaveshsingh519@gmail.com>
2022-05-21 16:37:06 +0530

    Basic intro to if else statement, also known as conditional statement

---
+++ src/basic_IF_ELSE.java
@@ -0,0 +1,37 @@
+public class basic_IF_ELSE {
+    public static void main (String[] args){
+        // Understanding of IF statement
+        /*
+            -->The IF statement is used to execute a certain line of code element is true
+            -->example :  if(condition){
+                                 if the condition is true executing this code.}
+         */
+
+        // Understanding of else statement
+        /*
+            -->This statement is used with the IF statement, it's executed when the if statement is false
+            -->example :  if(condition){
+                                 if the condition is true execute this code
+                                }
+                          else{
+                                 execute this one
+                                 }
+
+         */
+
+        // Understanding of else if statement
+        /*
+        -->This statement is used with the IF statement, it's executed when the if statement is false
+            -->example :  if(condition){
+                                 if the condition is true execute this code
+                                }
+                          else if(condition){
+                                 if this condition is true then execute this code
+                                 }
+                          else{
+                                 execute this one
+                                 }
+
+         */
+    }
+}
