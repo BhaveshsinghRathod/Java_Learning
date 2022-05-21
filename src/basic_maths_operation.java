1d5c9fcd9bae1fc3f7d8beab3f172a9695d41f74
Bhaveshsingh Rathod <rathodbhaveshsingh519@gmail.com>
2022-05-21 15:25:20 +0530

    Some of the basic maths operations in Java.

---
+++ src/basic_maths_operation.java
@@ -0,0 +1,20 @@
+public class basic_maths_operation {
+    public static void main(String[] args){
+        System.out.println(5+10);
+        System.out.println(10/5);
+        System.out.println(5*10);
+        double var1 = 12;
+        double var2 = 5;
+        System.out.println(var1/var2);   // If we don't devide then we are good with int.
+        System.out.println(var1*var2);
+        // We can not only store numbers but also store operations in the variables
+        double var3 = var1 + var2;
+        System.out.println(var3);
+        // if we use int data type then, we won't get answers in decimal.
+        String var4 = "Heyy there ";
+        String var5 = "i am fucking 20 years old";
+        String var6 = var4 + var5;
+        System.out.println(var4 + var5);  // when we add strings then statements in both the strings get added.
+        System.out.println(var6);
+    }
+}
