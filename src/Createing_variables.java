93b452a1eea65c19c087e305b6a55dca917aa7d4
Bhaveshsingh Rathod <rathodbhaveshsingh519@gmail.com>
2022-05-21 13:23:06 +0530

    example of data-type

---
+++ src/Createing_variables.java
@@ -0,0 +1,19 @@
+public class Createing_variables {
+    public static void main (String[] args){
+        // char
+        char var1 = 'a';
+        // we cannot give more than one value to char like string
+        String var2 = "Hi my name is xyz";
+        // we can also give a numerical value to the char and string but cannot perform any operation on them
+        boolean var3 = true;
+        int var4 = 9;
+        double var5 = 9.8;
+        float var6 = 9.5f;
+        System.out.println(var1);
+        System.out.println(var2);
+        System.out.println(var3);
+        System.out.println(var4);
+        System.out.println(var5);
+        System.out.println(var6);
+    }
+}
