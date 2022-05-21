1e50d97549aad42530b77efb9cb5fa3bbe412033
Bhaveshsingh Rathod <rathodbhaveshsingh519@gmail.com>
2022-05-21 10:44:38 +0530

    Intro to different Data types

---
+++ src/data_type.java
@@ -0,0 +1,13 @@
+public class data_type {
+    public static void main(String [] args){
+        // Boolean   <True or False>
+        // Byte Data Type <1) A primitive data type  2) It's value ranges between [-128 and 127]  3) It can be used instead of int  4) Example: "5">
+        // Char  <Data type used to store characters    example: "ABC">
+        // Short Data type   < Data type with a value between [-32768 and 32767]>
+        // Int Data type   < Data type value between [-2power 31 and 2power 31 -1]>
+        // Long Data type   < Data type value between [-2power 63 and 2power 63 -1]>
+        // Float Data type   < Data type with unlimited range but should not be used for precise values(a lot of decimals)>
+        // Double Data type  < Data type with unlimited range usually used for decimal>
+
+    }
+}
